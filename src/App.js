5e3e52e8b961bdca9121bb0b960a649f0d046e21
hrigg <rigg.heather6@gmail.com>
2022-09-13 14:43:21 -0400

    set up about page to render data from local host-backend

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import './App.css';
+import {Route, Routes,Switch} from 'react-router-dom'
+
+function App() {
+  const URL = "http://localhost:4000/";
+  return (
+    <div className="App">
+       <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects URL={URL} />} />
+        <Route path="/about" element={<About URL={URL} />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+const About = (props) => {
+    const [about, setAbout]= useState(null)
+    // const getAboutData= async () => {
+    //     const response = await fetch(props.URL+'about')
+    //     const data= await response.json()
+    // setAbout(data)
+    // }
+useEffect(()=> async function getAboutData(){
+    const response = await fetch(props.URL+'about')
+    const data= await response.json()
+setAbout(data)
+}, []
+)
+const loaded=()=>(
+    <div>
+        <h2> {about.name}</h2>
+        <h3> {about.email} </h3>
+        <p> {about.bio}</p>
+        <p> <img src={about.headshot} /></p>
+    </div>
+)
+  return  about ? loaded() : <h1> Loading...</h1>
+}
+
+export default About
